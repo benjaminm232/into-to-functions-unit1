040a5cce83c0cff08556221bec0f6c7304ac41d0
benjaminm232 <benjaminm232@nycstudents.net>
2026-09-16 06:20:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,36 +3,27 @@ from turtle import *
 t = Turtle()
 
 t.shape('turtle')
+t.speed(100)
 
-""" def square(x):
-    t.forward(x)
+""" def rectangle(x):
+    t.forward(125)
     t.left(90)
-    t.forward(x)
+    t.forward(100)
     t.left(90)
-    t.forward(x)
+    t.forward(125)
     t.left(90)
-    t.forward(x)
+    t.forward(100)
     t.left(90)
-square(200) """
+rectangle(200) """
 
-
-
-""" def equal(x):
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-    t.left(120)
-    t.forward(x)
-equal(200) 
-
-def right():
+for i in range(60):
     t.forward(100)
     t.left(90)
     t.forward(100)
-    t.left(135)
-    t.forward(142)
-right() """
-
-
-
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.right(5)
 turtle.done()
