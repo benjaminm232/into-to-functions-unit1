32636fbd2f99d60ad52b2ccb0ccb1c44e7feed96
benjaminm232 <benjaminm232@nycstudents.net>
2026-09-18 06:20:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ t.speed(100)
     t.left(90)
 rectangle(200) """
 
-def square(x):
+""" def square(x):
     t.forward(x)
     t.left(90)
     t.forward(x)
@@ -24,15 +24,27 @@ def square(x):
     t.forward(x)
     t.left(90)
     t.forward(x)
-    t.left(90)
+    t.left(90) 
 
-
-def doubleSquares(iRange):
-    length = 25
+def addSquares(iRange):
+    length = 5
     for i in range(iRange):
-        square(length)
-        length = length * 2
+        star(length)
+        length += 5
         t.right(5)
-doubleSquares(100)
+addSquares(60) """
+
+def star(x):
+    for i in range(5):
+        t.forward(150) 
+        t.right(144) 
+
+def addStar(iRange):
+    length = 5
+    for i in range(iRange):
+        star(length)
+        length += 5
+        t.right(5)
+addStar(60)
 
 turtle.done()
