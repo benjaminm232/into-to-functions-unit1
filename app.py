0459674c4fbe765fd6396dbb3e318b6aff68b8b3
benjaminm232 <benjaminm232@nycstudents.net>
2026-09-22 06:19:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ t = Turtle()
 t.shape('turtle')
 t.speed(100)
 
+""" t.forward(200) """
+""" def message(input):
+    print(input)
+message("Hello Class") """
+
+""" def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90) """
+
 """ def rectangle(x):
     t.forward(125)
     t.left(90)
@@ -15,6 +30,24 @@ t.speed(100)
     t.forward(100)
     t.left(90)
 rectangle(200) """
+
+""" def equal(x):
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+    t.left(120)
+    t.forward(x)
+equal(200) 
+
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(135)
+    t.forward(142)
+right() """
+
+# **i need to put the og circle loop here cause i deleted it
 
 """ def square(x):
     t.forward(x)
@@ -34,17 +67,29 @@ def addSquares(iRange):
         t.right(5)
 addSquares(60) """
 
-def star(x):
-    for i in range(5):
-        t.forward(150) 
-        t.right(144) 
+""" def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+length = 5
+for i in range (60):
+    square(length)
+    length += 5
+    t.right(5) """
 
-def addStar(iRange):
-    length = 5
-    for i in range(iRange):
-        star(length)
-        length += 5
-        t.right(5)
-addStar(60)
+""" def star(x):
+    for i in range(5):
+        t.forward(x) 
+        t.right(144)
+length = 5
+for i in range (60):
+    star(length)
+    length += 5
+    t.right(5) """
 
 turtle.done()
